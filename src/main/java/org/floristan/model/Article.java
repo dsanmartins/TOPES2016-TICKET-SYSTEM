b1a8d30d6343d5b7ab38698946e0104a5d184ed1
dsanmartins <dsanmartins@98m91.dc.ufscar.br>
2016-06-16 15:44:56 -0300

    Relationship with musical event.

--- src/main/java/org/floristan/model/Article.java
+++ src/main/java/org/floristan/model/Article.java
@@ -6,28 +6,35 @@ import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tshirt")
+@Table(name = "article")
 @Cacheable
-public class Tshirt implements Serializable {
+public class Article implements Serializable {
 
 
 	private static final long serialVersionUID = 1L; /**< Static property needed for JPA implementation. */ 
 	private int id;
 	private int size;
 	private String sexType;
+	private String type;
+	private float price;
+	private MusicalEvent musicalEvent;
 	
-	public Tshirt()
+	public Article()
 	{
 		
 	}
 	
-	public Tshirt(int size, String sexType) {
+	public Article(int size, String sexType, String type, float price, MusicalEvent musicalEvent) {
 		super();
 		this.size = size;
 		this.sexType = sexType;
+		this.type = type;
+		this.price = price;
+		this.musicalEvent = musicalEvent;
 	}
 
 	@Id
@@ -59,4 +66,34 @@ public class Tshirt implements Serializable {
 	public void setSexType(String sexType) {
 		this.sexType = sexType;
 	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	/**
+	 * Relacionamento muitos a um com a classe cadeira.
+	 * @return evento musical
+	 */
+	@ManyToOne(optional=true)
+	public MusicalEvent getMusicalEvent() {
+		return musicalEvent;
+	}
+
+	public void setMusicalEvent(MusicalEvent musicalEvent) {
+		this.musicalEvent = musicalEvent;
+	}
+	
 }
